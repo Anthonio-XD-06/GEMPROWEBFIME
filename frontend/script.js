488ff274b07410b5923f431203e3a0ef470ef0e8
Anthonio-XD-06 <160370226+Anthonio-XD-06@users.noreply.github.com>
2025-03-08 11:38:56 -0600

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,3 @@
-
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
-
-
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 
