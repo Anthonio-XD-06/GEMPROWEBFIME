7661bcf5fb5da73ed116dd1a28c63777d6431a63
Anthonio-XD-06 <160370226+Anthonio-XD-06@users.noreply.github.com>
2025-03-08 11:02:31 -0600

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -9,8 +9,6 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
