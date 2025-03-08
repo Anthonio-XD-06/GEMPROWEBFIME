f4e99695dfcf9c29da8117580be772e6dfe13aaf
Anthonio-XD-06 <160370226+Anthonio-XD-06@users.noreply.github.com>
2025-03-08 12:20:07 -0600

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -9,14 +9,15 @@
 document.addEventListener('DOMContentLoaded', function(){    
     //iniciar firebase
     const firebaseConfig = {
-    apiKey: "AIzaSyD7cUNhq1-tF-oeht0MmvlydTU-4LNf7_g",
-    authDomain: "prowebfimeproyecto.firebaseapp.com",
-    projectId: "prowebfimeproyecto",
-    storageBucket: "prowebfimeproyecto.firebasestorage.app",
-    messagingSenderId: "1088588778289",
-    appId: "1:1088588778289:web:55b92b8de5c654f620fe10",
-    measurementId: "G-YHB1RK6D7S"
+      apiKey: "AIzaSyD7cUNhq1-tF-oeht0MmvlydTU-4LNf7_g",
+      authDomain: "prowebfimeproyecto.firebaseapp.com",
+      projectId: "prowebfimeproyecto",
+      storageBucket: "prowebfimeproyecto.firebasestorage.app",
+      messagingSenderId: "1088588778289",
+      appId: "1:1088588778289:web:55b92b8de5c654f620fe10",
+      measurementId: "G-YHB1RK6D7S"
     };
+
     
     const app = initializeApp(firebaseConfig);
     const auth = firebase.auth(app);
