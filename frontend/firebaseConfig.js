f6245338ea1823db829d85d8a7444c0c11dc3a99
Anthonio-XD-06 <160370226+Anthonio-XD-06@users.noreply.github.com>
2025-03-09 12:16:28 -0600

    Create firebaseConfig.js

---
+++ frontend/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD7cUNhq1-tF-oeht0MmvlydTU-4LNf7_g",
+  authDomain: "prowebfimeproyecto.firebaseapp.com",
+  projectId: "prowebfimeproyecto",
+  storageBucket: "prowebfimeproyecto.firebasestorage.app",
+  messagingSenderId: "1088588778289",
+  appId: "1:1088588778289:web:55b92b8de5c654f620fe10",
+  measurementId: "G-YHB1RK6D7S"
+};
+
+window.firebaseConfig = firebaseConfig;
